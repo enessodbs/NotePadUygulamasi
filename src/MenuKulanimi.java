765f9c2edb0fc2f39de2a392c09d43742f3835e9
Enes Odabaş <enesodabas5234@gmail.com>
2024-01-13 00:14:10 +0300

    Git Commit Denem

--- src/MenuKulanimi.java
+++ src/MenuKulanimi.java
@@ -153,6 +153,7 @@ public class MenuKulanimi extends javax.swing.JFrame {
         pack();
     }// </editor-fold>//GEN-END:initComponents
     //Git Deneme
+    //Git2
     private void jMenuItem1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jMenuItem1ActionPerformed
         JFileChooser fc = new JFileChooser();
         int i = fc.showOpenDialog(this);
